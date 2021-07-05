2a9d88c934893db9281fd7a404a6f68bc0c6683f
jiangyu5 <528802378@qq.com>
2021-07-05 14:28:38 +0800

    删除按键按钮
    1.删除了按键按钮；
    2.增加了回车确认事件。

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,26 +1,31 @@
 const question = document.querySelector('label');
 const inputList = document.querySelectorAll('input');
-const btnList = document.querySelectorAll('button');
+const startButton = document.querySelector('button');
 const questionPre = document.getElementById('question-pre');
 
-// 根据输入的范围生成一个随机数字
+// 答题计数
+let quesAll = 0;
+let quesTrue = 0;
+
+// 根据填入的数字生成一个随机数
 function randomNum(){
     var minNum = Number(inputList[0].value);
     var maxNum = Number(inputList[1].value);
     return Math.floor(Math.floor(Math.random()*10) * (maxNum-minNum)/9) + minNum;
-} 
-
-// 替换题目
-function replaceNum() {
-    question.textContent = randomNum() + ' x ' + randomNum() + ' = ';
 }
 
-// 题量与答对计数
-let quesAll = Number(document.getElementById('ques-all').textContent);
-let quesTrue = Number(document.getElementById('ques-true').textContent);
+// 重置按钮
+function start() {
+    question.textContent = randomNum() + ' x ' + randomNum() + ' = ';
+    questionPre.textContent = '重置成功';
+    document.getElementById('ques-all').textContent = 0;
+    document.getElementById('ques-true').textContent = 0;
+    document.getElementById('ques-accuracy').textContent = 0;
+    quesAll = 0;
+    quesTrue = 0;
+}
 
-// 10条历史记录
-
+// 更新历史记录函数
 function replaceHistoty(history) {
     var historyUl = document.getElementById('history');
     var ulList = historyUl.querySelectorAll('li');
@@ -31,78 +36,45 @@ function replaceHistoty(history) {
     var liNode = document.createTextNode(history);
     newLi.append(liNode);
     historyUl.appendChild(newLi);
-
-    console.log(historyUl);
 }
 
-// 开始按钮
-function startButton() {
-    if (btnList[0].textContent === '开始') {
-        btnList[0].textContent = '重置';
-        replaceNum();
-    } else {
-        btnList[0].textContent = '开始';
-        document.getElementById('ques-all').textContent = 0;
-        document.getElementById('ques-true').textContent = 0;
-        document.getElementById('ques-accuracy').textContent = 0;
-    }
-}
-
-btnList[0].addEventListener('click', startButton);
-
-// 数字按钮
-btnList[1].onclick = function() { inputList[2].value += btnList[1].value; };
-btnList[2].onclick = function() { inputList[2].value += btnList[2].value; };
-btnList[3].onclick = function() { inputList[2].value += btnList[3].value; };
-btnList[4].onclick = function() { inputList[2].value += btnList[4].value; };
-btnList[5].onclick = function() { inputList[2].value += btnList[5].value; };
-btnList[6].onclick = function() { inputList[2].value += btnList[6].value; };
-btnList[7].onclick = function() { inputList[2].value += btnList[7].value; };
-btnList[8].onclick = function() { inputList[2].value += btnList[8].value; };
-btnList[9].onclick = function() { inputList[2].value += btnList[9].value; };
-btnList[10].onclick = function() { inputList[2].value += btnList[10].value; };
-btnList[11].onclick = function() { inputList[2].value += btnList[11].value; };
-btnList[12].onclick = function() { inputList[2].value += btnList[12].value; };
-
-// ok按钮
-btnList[16].onclick = function() {
+// 回车键
+function ok() {
+    quesAll += 1; // 总题量 +1
     var qTxt = question.textContent;
-    // 把问题前置一个
-    questionPre.textContent = qTxt + inputList[2].value;
+    questionPre.textContent = qTxt + inputList[2].value; // 把提交的前置一个
+    quesAll += 1; // 总题数 +1
+    document.getElementById('ques-all').textContent = quesAll;
     qTxt = qTxt.replace(' x ', '*');
     qTxt = qTxt.replace(' = ', '');
 
-    quesAll += 1;
-    document.getElementById('ques-all').textContent = quesAll;
+    // 正确：正确数更新，前置答题追加 【正确提示】；否则 前置的答题追加【错误提示】
     if (eval(qTxt) === Number(inputList[2].value)) {
         questionPre.textContent += ' 正确';
-        quesTrue += 1;
+        quesTrue += 1; // 正确数 +1
         document.getElementById('ques-true').textContent = quesTrue;
     } else {
         questionPre.textContent += ' 错误 答案:' + eval(qTxt);
     }
+
+    // 修改正确率
     document.getElementById('ques-accuracy').textContent = Math.round(quesTrue/quesAll*10000)/100 + '%';
+
+    // 更新题目
     inputList[2].value = '';
-    replaceNum();
+    question.textContent = randomNum() + ' x ' + randomNum() + ' = ';
+
+    // 更新历史记录
     replaceHistoty(questionPre.textContent);
 }
 
-// del按钮
-btnList[13].onclick = function() {
-    var valueTxt = inputList[2].value;
-    inputList[2].value = valueTxt.slice(0, valueTxt.length-1);
+function keyOk(e) {
+    var e = e || window.event;  //标准化事件处理
+    if (e.keyCode === 13) {
+        ok();
+    }
 }
 
-// cl按钮
-btnList[14].onclick = function() {
-    inputList[2].value = '';
-}
 
-// auto按钮
-btnList[15].onclick = function() {
-    var qTxt = question.textContent;
-    qTxt = qTxt.replace(' x ', '*');
-    qTxt = qTxt.replace(' = ', '');
-    inputList[2].value = eval(qTxt);
-}
-
+startButton.addEventListener('click', start);
+inputList[2].onkeyup = keyOk;
